c5d6ea83e7d28e94bb41a77c95b6195ee9ecbd1d
Tri Ahmad Irfan <irfan@stoqo.com>
2017-12-09 14:15:34 +0700

    Fix Disambiguation Error

--- scraper.py
+++ scraper.py
@@ -9,25 +9,30 @@ class Scraper():
     def __init__(self, language):
         self.language = language
 
+    def get_first_page(self, possible_page_titles):
+        first_page = possible_page_titles[0]
+        page_result = wikipedia.page(first_page)
+        return page_result.content
+
     def get(self, query):
         if self.language == INDONESIAN:
             wikipedia.set_lang(ID)
         else:
             wikipedia.set_lang(EN)
-
-        possible_page_titles = wikipedia.search(query)
-        if len(possible_page_titles) > 0:
-            first_page = possible_page_titles[0]
-            page_result = wikipedia.page(first_page)
-            return page_result.content
-        else:
-            suggested_query = wikipedia.suggest(query)
-            possible_page_titles = wikipedia.search(suggested_query)
-
+        try:
+            possible_page_titles = wikipedia.search(query)
             if len(possible_page_titles) > 0:
-                first_page = possible_page_titles[0]
-                page_result = wikipedia.page(first_page)
-                return page_result.content
+                return self.get_first_page(possible_page_titles)
+            else:
+                suggested_query = wikipedia.suggest(query)
+                possible_page_titles = wikipedia.search(suggested_query)
+
+                if len(possible_page_titles) > 0:
+                    return self.get_first_page(possible_page_titles)
+
+        except wikipedia.exceptions.DisambiguationError as e:
+            possible_page_titles = e.options
+            return self.get_first_page(possible_page_titles)
 
         return None
 
