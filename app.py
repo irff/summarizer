42e99df8b8902a2fb7100e638ee21299a991d61b
M46F <muhammadassagaf46@gmail.com>
2017-12-09 21:14:54 +0700

    forgot to initiate Summarizer() tehee

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
-
+summarizer = Summarizer()
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 translator = Translator()
 
