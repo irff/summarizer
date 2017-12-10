73f431da708ac63a5d5dc031f1a28cff6f5ddf75
M46F <muhammadassagaf46@gmail.com>
2017-12-10 14:25:01 +0700

    some error handling

--- scraper.py
+++ scraper.py
@@ -55,22 +55,26 @@ class Scraper():
             lang = ENGLISH
         try:
             possible_page_titles = wikipedia.search(query)
-            if len(possible_page_titles) > 0:
+            if possible_page_titles and len(possible_page_titles) > 0:
                 return possible_page_titles[0], 0, lang
             else:
                 suggested_query = wikipedia.suggest(query)
-                if len(suggested_query) < 1:
+                if not suggested_query or len(suggested_query) < 1:
                     return [], -1, lang
                 possible_page_titles = wikipedia.search(suggested_query)
 
-                if len(possible_page_titles) > 0:
+                if possible_page_titles and len(possible_page_titles) > 0:
                     return possible_page_titles[0], -1, lang
 
         except wikipedia.exceptions.DisambiguationError as e:
             possible_page_titles = e.options
-            return possible_page_titles[0], 0, lang
+            if possible_page_titles and len(possible_page_titles) > 0:
+                return possible_page_titles[0], 0, lang
+            return [], -1, lang
         else:
-            return possible_page_titles[0], 0, lang
+            if possible_page_titles and len(possible_page_titles) > 0:
+                return possible_page_titles[0], 0, lang
+            return [], -1, lang
 
         return [], -1, lang
     def get_intro_lang(self, query, lang):
