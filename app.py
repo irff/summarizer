44e870aa45e0fb3dd05c13541dc1c0349491927f
Muhammad <muhammadassagaf46@gmail.com>
2017-12-17 10:08:32 +0000

    add stuff for gunicorn

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ if __name__ == "__main__":
     # app.run(debug=options.debug, port=options.port)
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
