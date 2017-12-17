6585fdf666f202c8d3431ec384d5791cfbce0321
M46F <muhammadassagaf46@gmail.com>
2017-12-17 17:21:11 +0700

    change to need prefix mbok.

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def handle_text_message(event):
 
     if cmd[0] == '/check' and len(cmd) > 1:
         reply(event,random.randint(0,int(cmd[1])))
-    elif '?' in text and flag:
+    elif '?' in text and flag and text.startswith('mbok,'):
         print("ini detectednya :" + str(translator.detect(text).lang))
         language = 'indonesian' if (translator.detect(text).lang == 'id' or translator.detect(text).lang == 'msid' or translator.detect(text).lang == 'idms') else 'english'
         print(language)
